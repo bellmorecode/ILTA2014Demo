d942ed049c886e435096816ede46fb3f64936b7c
Glenn <glenn.ferrie@gmail.com>
2014-05-20 12:14:29 -0400

    finished data access code to documents area

--- DemoWebApp/DemoWebApp/Controllers/HomeController.cs
+++ DemoWebApp/DemoWebApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.SharePoint.Client;
+﻿using DemoWebApp.Models;
+using Microsoft.SharePoint.Client;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -22,6 +23,8 @@ namespace DemoWebApp.Controllers
             var user = ConfigurationManager.AppSettings["userlogin"];
             var pwd = ConfigurationManager.AppSettings["password"];
 
+
+
             // encode password and set credentials
             SecureString passWord = new SecureString();
             foreach (char c in pwd.ToCharArray()) passWord.AppendChar(c);
@@ -33,16 +36,24 @@ namespace DemoWebApp.Controllers
             clientContext.Load(oListCollection);
             clientContext.ExecuteQuery();
 
+            //var folderlist = oListCollection.Select(q => new Document { Title = q.EntityTypeName, Url = "" }).ToList();
+
             // Get Items from Announcements List
             var announcementsList = oListCollection.First(q => q.EntityTypeName == "ILTA_x0020_AnnouncementsList");
             var items = announcementsList.GetItems(new CamlQuery { ViewXml = "<View><ViewFields  ><FieldRef Name='Title' /></ViewFields></View>" });
             clientContext.Load(items);
             clientContext.ExecuteQuery();
 
+            // Get Items from Documents List
+            var docsList = oListCollection.First(q => q.EntityTypeName == "Shared_x0020_Documents");
+            var docs = docsList.GetItems(new CamlQuery { ViewXml = "<View><ViewFields  ><FieldRef Name='DocIcon' /><FieldRef Name='FileRef' /><FieldRef Name='Title' /><FieldRef Name='ows_LinkFilename' /></ViewFields></View>" });
+            clientContext.Load(docs);
+            clientContext.ExecuteQuery();
+
             // create View Model
             var model = new Models.HomeViewModel();
-            model.Announcements = items.Select(q => new Models.Announcement { Title = q.FieldValues["Title"].ToString() }).ToList();
-
+            model.Announcements = items.Select(q => new Announcement { Title = q.FieldValues["Title"].ToString() }).ToList();
+            model.Documents = docs.Select(q => new Document { Title = q.FieldValues["FileLeafRef"].ToString(), Url = string.Format("{0}{1}", "https://somethingdigital.sharepoint.com", q.FieldValues["FileRef"].ToString())  }).ToList();
             return View(model);
         }
 
