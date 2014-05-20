ebf7f49015b030cc9f801fa3d328f702ad9d5277
Glenn <glenn.ferrie@gmail.com>
2014-05-20 01:34:29 -0400

    added comments

--- DemoWebApp/DemoWebApp/Controllers/HomeController.cs
+++ DemoWebApp/DemoWebApp/Controllers/HomeController.cs
@@ -13,26 +13,32 @@ namespace DemoWebApp.Controllers
     {
         public ActionResult Index()
         {
-             ClientContext clientContext = new ClientContext("https://somethingdigital.sharepoint.com/sites/spdev");
-             SecureString passWord = new SecureString();
+            // Get Announcements List from SharePoint Online
+            // instantiate client context
+            ClientContext clientContext = new ClientContext("https://somethingdigital.sharepoint.com/sites/spdev");   
+             
+            // encode password and set credentials
+            SecureString passWord = new SecureString();
+            foreach (char c in "yourpassword".ToCharArray()) passWord.AppendChar(c);
+            clientContext.Credentials = new SharePointOnlineCredentials("administrator@somethingdigital.onmicrosoft.com", passWord);
 
-             foreach (char c in "yourpassword".ToCharArray()) passWord.AppendChar(c);
+            // Get Lists from SharePoint
+            Web oWeb = clientContext.Web;
+            ListCollection oListCollection = oWeb.Lists;
+            clientContext.Load(oListCollection);
+            clientContext.ExecuteQuery();
 
-             clientContext.Credentials = new SharePointOnlineCredentials("administrator@somethingdigital.onmicrosoft.com", passWord);
-             Web oWeb = clientContext.Web;
-             ListCollection oListCollection = oWeb.Lists;
-             clientContext.Load(oListCollection);
-             clientContext.ExecuteQuery();
+            // Get Items from Announcements List
+            var announcementsList = oListCollection.First(q => q.EntityTypeName == "ILTA_x0020_AnnouncementsList");
+            var items = announcementsList.GetItems(new CamlQuery { ViewXml = "<View><ViewFields  ><FieldRef Name='Title' /></ViewFields></View>" });
+            clientContext.Load(items);
+            clientContext.ExecuteQuery();
 
-             var announcementsList = oListCollection.First(q => q.EntityTypeName == "ILTA_x0020_AnnouncementsList");
-             var items = announcementsList.GetItems(new CamlQuery { ViewXml = "<View><ViewFields  ><FieldRef Name='Title' /></ViewFields></View>" });
-             clientContext.Load(items);
-             clientContext.ExecuteQuery();
+            // create View Model
+            var model = new Models.HomeViewModel();
+            model.Announcements = items.Select(q => new Models.Announcement { Title = q.FieldValues["Title"].ToString() }).ToList();
 
-             var model = new Models.HomeViewModel();
-             model.Announcements = items.Select(q => new Models.Announcement { Title = q.FieldValues["Title"].ToString() }).ToList();
-
-             return View(model);
+            return View(model);
         }
 
         public ActionResult About()
